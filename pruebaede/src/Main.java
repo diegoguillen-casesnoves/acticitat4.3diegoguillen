40cd05b8856f783c3dd2fee7be7f9f62962c22c8
Diego Guillén <diegoguillen@paucasesnovescifp.cat>
2026-02-11 18:42:00 +0100

    Segundo commit

--- pruebaede/src/Main.java
+++ pruebaede/src/Main.java
@@ -2,4 +2,6 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
     System.out.println("Hola actividad 4.3 ");
+
+    System.out.println("segundo commit... ");
 }
